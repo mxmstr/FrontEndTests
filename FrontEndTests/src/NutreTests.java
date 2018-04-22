417b876e39be41c4332038fc0dd40ae768e82886
Eric Lynch <lynch.er18@gmail.com>
2018-04-22 16:11:34 -0400

    Change account name

--- FrontEndTests/src/NutreTests.java
+++ FrontEndTests/src/NutreTests.java
@@ -9,7 +9,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -18,6 +21,9 @@ public class NutreTests {
 	
 	private static String driverPath = "/users/ericlynch/documents/chromedriver";
 	private static String homePage = "http://dev.gonutre.com";
+	private static String email = "lynch.er18@gmail.com";
+	private static String password = "123123";
+	
 	private WebDriver driver;
 	private ArrayList<AsyncTester> threads;
 	
@@ -52,9 +58,9 @@ public class NutreTests {
 	    try {
 	    	driver.findElement(By.linkText("Log In")).click();
 		    driver.findElement(By.cssSelector("input[type='email']")).clear();
-		    driver.findElement(By.cssSelector("input[type='email']")).sendKeys("lynch.er18@gmail.com");
+		    driver.findElement(By.cssSelector("input[type='email']")).sendKeys(email);
 		    driver.findElement(By.cssSelector("input[type='password']")).clear();
-		    driver.findElement(By.cssSelector("input[type='password']")).sendKeys("123123");
+		    driver.findElement(By.cssSelector("input[type='password']")).sendKeys(password);
 		    driver.findElement(By.xpath("//button[contains(.,'Log In')]")).click();
 		    
 			Thread.sleep(500);
@@ -71,7 +77,16 @@ public class NutreTests {
 		System.setProperty("webdriver.chrome.driver", driverPath);
 		
 		threads = new ArrayList<AsyncTester>();
-		driver = new ChromeDriver();
+		
+		while (true) {
+			
+			try {
+				driver = new ChromeDriver();
+				break;
+			}
+			catch (WebDriverException e) {}
+			
+		}
         
         driver.manage().window().maximize();
         driver.manage().window().setSize(
@@ -93,66 +108,107 @@ public class NutreTests {
 		
 	}
 	
-	private void add10ItemsToCart() {
+	private void add10ItemsToCart() throws InterruptedException {
 		
 	    driver.findElement(By.linkText("A la Carte")).click();
 	    driver.findElement(By.xpath("//label[contains(.,'Dinner')]")).click();
-	    driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div[2]/div/div/div[2]/label[3]")).click();
+	    
+	    Thread.sleep(1000);
+	    
 	    new Select(driver.findElement(By.cssSelector("select"))).selectByVisibleText("5");
 	    new Select(driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div/div/select"))).selectByVisibleText("5");
+	    
+	    Thread.sleep(1000);
+	    
 	    driver.findElement(By.cssSelector("button[name=\"Soup 1\"]")).click();
 	    driver.findElement(By.cssSelector("button[name=\"Pizza\"]")).click();
-	    //driver.findElement(By.xpath("//div[@id='root']/div/div/div/div/div/div/div/button")).click();
 	    
 	}
 	
+	private void redeemPromoCode(String code) {
+			
+		driver.findElement(By.cssSelector("input.pt-input.pt-fill")).clear();
+	    driver.findElement(By.cssSelector("input.pt-input.pt-fill")).sendKeys(code);
+	    driver.findElement(By.cssSelector("button.cart__promocode-btn")).click();
+    	
+	}
+	
 	//@Test
-	public void Menu() {
+	public void checkoutItems() throws InterruptedException {
 		
-		new Menu(driver, homePage);
-        joinThreads();
+		String bodyText;
+		
+    	add10ItemsToCart();
+    	Thread.sleep(1000);
+    	
+    	driver.findElement(By.cssSelector(
+				"button.pt-button.pt-minimal.pt-icon-shopping-cart.topbar__cart-btn.topbar__ma-top-8")).click();
+    	Thread.sleep(1000);
+    	
+    	
+    	redeemPromoCode("123123");
+    	Thread.sleep(1000);
+    	
+    	bodyText = driver.findElement(By.tagName("body")).getText();
+    	Assert.assertTrue("Used code accepted!", bodyText.contains("Code is invalid or already used."));
+    	
+    	
+    	redeemPromoCode("abc");
+    	Thread.sleep(1000);
+    	
+    	bodyText = driver.findElement(By.tagName("body")).getText();
+    	Assert.assertTrue("Invalid code accepted!", bodyText.contains("Code is invalid or already used."));
+		
+    	
+    	Assert.assertTrue(
+    			"Checkout without shipping info!",
+    			!driver.findElement(By.cssSelector("button.cart__checkout-btn")).isEnabled());
+    	
+    	
+    	driver.findElement(By.xpath("//button[@type='button']")).click();
+    	driver.findElement(By.xpath("(//button[@type='button'])[3]")).click();
+    	//driver.findElement(By.name("Apply")).click();
+        driver.findElement(By.cssSelector("button.cart__checkout-btn")).click();
+        driver.findElement(By.name("cardnumber")).clear();
+        driver.findElement(By.name("cardnumber")).sendKeys("4242 4242 4242 4242");
+        driver.findElement(By.name("exp-date")).clear();
+        driver.findElement(By.name("exp-date")).sendKeys("‎02 / ‎20");
+        driver.findElement(By.name("cvc")).clear();
+        driver.findElement(By.name("cvc")).sendKeys("123");
+        driver.findElement(By.name("postal")).clear();
+        driver.findElement(By.name("postal")).sendKeys("02116");
+        driver.findElement(By.xpath("//form/div/div/button")).click();
+    	
+	}
+	
+	private void changeAccountName(String name) {
+		
+		driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div/div/div[2]/h2/span")).click();
+		
+	    WebElement nameField = driver.findElement(By.cssSelector("div.content__input-ma:nth-child(1) > div:nth-child(2) > input:nth-child(1)"));
+	    nameField.clear();
+	    nameField.sendKeys(name);
+	    
+	    driver.findElement(By.cssSelector("button.nu-button-h.content__button")).click();
+	    driver.findElement(By.xpath("//div[@id='root']/div/div/div[2]/div/div/div[2]/h2/span")).click();
 		
 	}
 	
 	@Test
-	public void Promo() {
+	public void changeAccountDetails() throws InterruptedException {
 		
+		driver.findElement(By.linkText("Account")).click();
+    	Thread.sleep(1000);
 		
-		
-	    try {
-	    	
-	    	String bodyText;
-	    	
-	    	add10ItemsToCart();
-	    	
-	    	Thread.sleep(2000);
-	    	
-	    	driver.findElement(By.cssSelector(
-					"button.pt-button.pt-minimal.pt-icon-shopping-cart.topbar__cart-btn.topbar__ma-top-8")).click();
-			
-	    	driver.findElement(By.cssSelector("input.pt-input.pt-fill")).clear();
-		    driver.findElement(By.cssSelector("input.pt-input.pt-fill")).sendKeys("123123");
-		    driver.findElement(By.cssSelector("button.cart__promocode-btn")).click();
-	    	
-			Thread.sleep(2000);
-			
-			bodyText = driver.findElement(By.tagName("body")).getText();
-	    	Assert.assertTrue("Used code accepted!", bodyText.contains("Code is invalid or already used."));
-	    	
-	    	driver.findElement(By.cssSelector("input.pt-input.pt-fill")).clear();
-		    driver.findElement(By.cssSelector("input.pt-input.pt-fill")).sendKeys("abc");
-		    driver.findElement(By.cssSelector("button.cart__promocode-btn")).click();
-	    	
-			Thread.sleep(2000);
-			
-			bodyText = driver.findElement(By.tagName("body")).getText();
-	    	Assert.assertTrue("Invalid code accepted!", bodyText.contains("Code is invalid or already used."));
-		
-		} 
-	    catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-    	
+	    changeAccountName("Joe");
+	    
+	    Assert.assertTrue(
+	    		"Name was not updated!",
+	    		driver.findElement(By.cssSelector(
+	    				"div.row:nth-child(3) > div:nth-child(2)")).getText().contains("Joe"));
+	    
+	    changeAccountName("John");
+	    
 	}
 	
 	@After
